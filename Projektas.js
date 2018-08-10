f0fd37fa4b4e4dbee527929907b48f7277a87f50
AstaHome <42231461+AstaHome@users.noreply.github.com>
2018-08-10 18:29:04 +0300

    Add new section

--- Projektas.js
+++ Projektas.js
@@ -1,15 +1,21 @@
-$(document).ready(function(){
-    $('.carousel_1').owlCarousel({
+jQuery(document).ready(function(){
+    jQuery('.carousel_1').owlCarousel({
         margin:10,
         loop:true,
         autoWidth:true,
         items:4
     })
 
-    $('.carousel_2').owlCarousel({
+    jQuery('.carousel_2').owlCarousel({
         margin:10,
         loop:true,
         autoWidth:true,
         items:4
     })
   });
+
+jQuery(document).ready(function(){
+    jQuery('.burger_nav').click(function(){
+        jQuery('.nav').toggleClass('show');
+    });
+});
